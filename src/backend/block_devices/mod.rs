9cb2bc199c6ef2e79807d4d7b6876e8220d902fd
Anton Gepting <anton.gepting@gmail.com>
2023-10-28 16:50:56 +0200

    chore(block): add rustix sub mod

--- src/backend/block_devices/mod.rs
+++ src/backend/block_devices/mod.rs
@@ -3,6 +3,7 @@ pub mod libc_mtab;
 pub mod libc_statvfs;
 pub mod nix_statvfs;
 pub mod proc_mountinfo;
+pub mod rustix_statvfs;
 pub mod sys_block;
 
 pub use etc_mtab::*;
@@ -10,4 +11,5 @@ pub use libc_mtab::*;
 pub use libc_statvfs::*;
 pub use nix_statvfs::*;
 pub use proc_mountinfo::*;
+pub use rustix_statvfs::*;
 pub use sys_block::*;
