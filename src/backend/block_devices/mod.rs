610eafa450a1ae1ed36bd37c8b8f8776fe8c521f
Anton Gepting <anton.gepting@gmail.com>
2023-11-09 10:50:24 +0100

    chore: add mtab mod

--- src/backend/block_devices/mod.rs
+++ src/backend/block_devices/mod.rs
@@ -4,6 +4,7 @@ pub mod libc_statvfs;
 pub mod nix_statvfs;
 pub mod proc_mountinfo;
 pub mod rustix_statvfs;
+pub mod self_mtab;
 pub mod sys_block;
 
 pub use etc_mtab::*;
@@ -12,4 +13,5 @@ pub use libc_statvfs::*;
 pub use nix_statvfs::*;
 pub use proc_mountinfo::*;
 pub use rustix_statvfs::*;
+pub use self_mtab::*;
 pub use sys_block::*;
