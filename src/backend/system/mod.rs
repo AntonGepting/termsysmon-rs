920200e6c2dc6df4988b390b0b99734ab66c44e0
Anton Gepting <anton.gepting@gmail.com>
2024-01-22 19:19:04 +0100

    chore(backend): remove libc, nix lib code usage

--- src/backend/system/mod.rs
+++ src/backend/system/mod.rs
@@ -1,13 +1,9 @@
-pub mod libc_sysinfo;
-pub mod nix_sysinfo;
 pub mod proc_cpuinfo;
 pub mod proc_meminfo;
 pub mod proc_stat;
 pub mod process;
 pub mod sys_class_dmi;
 
-pub use libc_sysinfo::*;
-pub use nix_sysinfo::*;
 pub use proc_cpuinfo::*;
 pub use proc_meminfo::*;
 pub use proc_stat::*;
