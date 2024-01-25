517f64f0d30f0690012965de3b53372eca067cbb
Anton Gepting <anton.gepting@gmail.com>
2024-01-25 16:13:01 +0100

    chore(net): remove libc dependency

--- src/backend/network/mod.rs
+++ src/backend/network/mod.rs
@@ -1,9 +1,7 @@
-// pub mod libc_ifaddrs;
 pub mod nix_network;
 pub mod proc_net_dev;
 // pub mod sys_class_net;
 
-// pub use libc_ifaddrs::*;
 pub use nix_network::*;
 pub use proc_net_dev::*;
 // pub use sys_class_net::*;
